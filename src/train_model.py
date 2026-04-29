c8e9c28e46c8afae9d5af62a9608bde154e7400a
ranjanvaibhawi <ranjanvaibhawi@gmail.com>
2026-04-30 00:15:52 +0530

    Enhanced model training with layer unfreezing and tuning

--- src/train_model.py
+++ src/train_model.py
@@ -4,6 +4,7 @@ from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Dropout
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import Adam
 
 IMG_SIZE = (224, 224)
 BATCH_SIZE = 8
@@ -15,9 +16,13 @@ TEST_DIR  = "./data/frames_split/test"
 # Data generators
 train_gen = ImageDataGenerator(
     rescale=1./255,
-    rotation_range=10,
+    # rotation_range=10,
+    rotation_range=12,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
     horizontal_flip=True,
-    zoom_range=0.1
+    brightness_range=[0.8,1.2],
+    zoom_range=0.15
 )
 
 val_gen = ImageDataGenerator(rescale=1./255)
@@ -52,21 +57,32 @@ base_model = VGG16(
     input_shape=(224,224,3)
 )
 
-for layer in base_model.layers:
+# for layer in base_model.layers:
+#     layer.trainable = False
+
+for layer in base_model.layers[:-2]:
     layer.trainable = False
+
+for layer in base_model.layers[-2:]:
+    layer.trainable = True
 
 # Model
 model = Sequential([
     base_model,
     Flatten(),
     Dense(128, activation='relu'),
-    Dropout(0.5),
+    Dropout(0.6),
     Dense(2, activation='softmax')
 ])
 
+# model.compile(
+#     optimizer='adam',
+#     loss='categorical_crossentropy',
+#     metrics=['accuracy']
+# )
 model.compile(
-    optimizer='adam',
-    loss='categorical_crossentropy',
+    optimizer=Adam(learning_rate=5e-6),
+    loss='binary_crossentropy',
     metrics=['accuracy']
 )
 
