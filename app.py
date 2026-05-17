ddbb25ce6017bb847deba33a31a53ba99e5f31a3
ranjanvaibhawi <ranjanvaibhawi@gmail.com>
2026-05-17 20:47:10 +0530

    Integrated app.py with logistic regression model

--- app.py
+++ app.py
@@ -1,83 +1,58 @@
 from flask import Flask, render_template, request
 from flask import send_from_directory
-import os
-import cv2
-import numpy as np
-from tensorflow.keras.models import load_model
 
+import os
+
+from src.paper_pipeline import analyze_video
+
+# Flask App Setup
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "uploads"
+
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# Load model once
-model = load_model("./models/vgg16_ffpp.keras")
-
-IMG_SIZE = 224
-
-def predict_video(video_path):
-    cap = cv2.VideoCapture(video_path)
-
-    predictions = []
-    count = 0
-
-    while True:
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-
-        # Sample every 15th frame
-        if count % 15 == 0:
-            frame = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
-            frame = frame / 255.0
-            frame = np.expand_dims(frame, axis=0)
-
-            pred = model.predict(frame, verbose=0)[0][0]
-
-            predictions.append(pred)
-
-        count += 1
-
-        if len(predictions) >= 10:
-            break
-
-    cap.release()
-
-    avg_pred = np.mean(predictions)
-
-    if avg_pred > 0.5:
-        return "FAKE", round(avg_pred * 100, 2)
-    else:
-        return "REAL", round((1 - avg_pred) * 100, 2)
-
-
+# Home Route
 @app.route("/")
 def home():
+
     return render_template("index.html")
 
+# Uploaded Video Route
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+    return send_from_directory(
+        app.config['UPLOAD_FOLDER'],
+        filename
+    )
+
+# Prediction Route
 @app.route("/predict", methods=["POST"])
 def predict():
+
     file = request.files["video"]
 
-    filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+    filepath = os.path.join(
+        app.config["UPLOAD_FOLDER"],
+        file.filename
+    )
+
     file.save(filepath)
 
-    result, confidence = predict_video(filepath)
+    # Final Logistic Regression Pipeline
+    result, confidence = analyze_video(filepath)
 
     return render_template(
         "result.html",
         result=result,
         confidence=confidence,
         filename=file.filename,
-        video_path=filepath
+        video_path=file.filename
     )
 
-
 if __name__ == "__main__":
+
     app.run(debug=True)
